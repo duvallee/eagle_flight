978df7e3d09bb53e33bb04fdb7ff11ac0a5302f2
duvallee <duvallee@nate.com>
2018-11-29 17:24:28 +0900

    add mpu regions for discovery_stm32f7

--- src/main.cpp
+++ src/main.cpp
@@ -70,6 +70,20 @@ static void MPU_Config(void)
 
    HAL_MPU_ConfigRegion(&MPU_InitStruct);
 
+   // Enable D-cache on SDRAM (Write-through)
+   MPU_InitStruct.Enable                                 = MPU_REGION_ENABLE;
+   MPU_InitStruct.BaseAddress                            = 0xC0000000;
+   MPU_InitStruct.Size                                   = MPU_REGION_SIZE_8MB;
+   MPU_InitStruct.AccessPermission                       = MPU_REGION_FULL_ACCESS;
+   MPU_InitStruct.IsBufferable                           = MPU_ACCESS_NOT_BUFFERABLE;
+   MPU_InitStruct.IsCacheable                            = MPU_ACCESS_CACHEABLE;
+   MPU_InitStruct.IsShareable                            = MPU_ACCESS_SHAREABLE;
+   MPU_InitStruct.Number                                 = MPU_REGION_NUMBER0;
+   MPU_InitStruct.TypeExtField                           = MPU_TEX_LEVEL0;
+   MPU_InitStruct.SubRegionDisable                       = 0x00;
+   MPU_InitStruct.DisableExec                            = MPU_INSTRUCTION_ACCESS_DISABLE;
+   HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
    /* Enable the MPU */
    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 }
